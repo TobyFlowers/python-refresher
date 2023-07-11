8f924408ca5e4316e6ecebf3abfcc625a62b79f2
tobyflowers <tobiasgflowers@gmail.com>
2023-07-11 15:47:44 -0400

    All Tests

--- test_hello.py
+++ test_hello.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 import hello
 
@@ -5,6 +6,32 @@ import hello
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello.hello(), "Hello, world!")
+
+    def test_add(self):
+        self.assertEqual(hello.add(1,2), 3)
+
+    def test_sub(self):
+        self.assertEqual(hello.sub(2,2), 0)   
+
+    def test_mul(self):
+        self.assertEqual(hello.mul(1,2), 2)
+
+    def test_div(self):
+        self.assertEqual(hello.div(4,2), 2)
+
+    def test_sqrt(self):
+        self.assertEqual(hello.sqrt(4), 2)
+
+    def test_power(self):
+        self.assertEqual(hello.power(1,2), 1)
+
+    def test_log(self):
+        self.assertEqual(hello.log(10), 2.302585092994046)
+
+    def test_exp(self):
+        self.assertEqual(hello.exp(1), math.e)
+
+    
 
     def test_sin(self):
         self.assertEqual(hello.sin(0), 0)
