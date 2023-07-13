c0c70c58e551eee2d65a0fd1bdcac56e7321d763
tobyflowers <tobiasgflowers@gmail.com>
2023-07-13 11:06:33 -0400

    fixing dotcoms

--- physics.py
+++ physics.py
@@ -2,23 +2,18 @@ d_water = 1000  # density of water -> 1 m^3 = 1000kg
 g = 9.81  # acceleration due to gravity (m/s^2)
 
 
-"""
-calculates the bouyancy of an object in any given fluid density
-units: newtons
-"""
-
-
 def calculate_bouyancy(v, density_fluid):
+    """
+    calculates the bouyancy of an object in any given fluid density
+    units: newtons"""
     if v <= 0 or density_fluid <= 0:
         raise ValueError("Volume and fluid density must both be positive!")
     return density_fluid * g * v
 
 
-"""calculates if an object with a given volume and mass will float or not in water
-true/false"""
-
-
 def will_it_float(v, mass):
+    """calculates if an object with a given volume and mass will float or not in water
+    true/false"""
     if v <= 0 or mass <= 0:
         raise ValueError("Volume and mass must both be positive!")
     if (calculate_bouyancy(v, d_water) - (g * mass)) == 0:
@@ -26,12 +21,9 @@ def will_it_float(v, mass):
     return (calculate_bouyancy(v, d_water) - (g * mass)) > 0
 
 
-"""
-calculates the pressure at any given depth in water.
-units: pascals
-"""
-
-
 def calculate_pressure(depth):
+    """
+    calculates the pressure at any given depth in water.
+    units: pascals"""
     depth = abs(depth)
     return depth * g * d_water
